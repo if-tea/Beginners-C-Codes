36ae058632dea52884576267ec9ab25537946289
Md Mehedi Islam Khan Ifti <54150906+if-tea@users.noreply.github.com>
2023-01-07 00:03:38 +0600

    first code upload

---
+++ Palindrome_checking.c
@@ -0,0 +1,29 @@
+
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+
+    int num, test, remainder, memory=0;
+
+
+    printf("Enter a number:");
+    scanf("%d", &num);
+    test= num;
+
+    while(num>0){
+
+        remainder=num%10;
+        memory=(memory*10)+remainder;
+        num=num/10;
+    }
+
+    printf("Reversed: %d", memory);
+
+    if(test==memory)
+        printf("\n\n%d is a palindrome number.", test);
+    else
+        printf("\n\n%d is not a Palindrome number.", test);
+
+    return 0;
+}
